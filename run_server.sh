3f5013a763254566e7aa538555f7e88f50b3605d
vmusulainen <vmusulainen@gmail.com>
2020-12-17 19:19:30 +0300

    - update sh files

--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-export GIT_TOKEN=<your token here>
-export SEARCHIFY_API_URL=<your token here>
+export GIT_TOKEN=<your github token here>
+export SEARCHIFY_API_URL=<your searchify token here>
 rake server
